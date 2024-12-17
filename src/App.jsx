c2975398dd256b07c93e4befb9440e5cc06a21cb
shivam <shivammahrolia@gmail.com>
2024-12-17 12:54:32 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
             }
           />
           <Route
-            path="anna"
+            path="productvoice"
             element={
               // <ProtectedRoute>
                 <Voice />
