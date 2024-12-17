fd12185a850a64e5fbb4a10f2aaaea83c0260d4c
shivam <shivammahrolia@gmail.com>
2024-12-17 12:39:18 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Products from "./pages/Products/Products";
 import Voice from "./pages/Voice/Voice";
 
 function App() {
-    return (
-        <HashRouter>
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route index element={<Navigate replace to="dashboard" />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="product" element={<Products />} />
-                    <Route path="anna" element={<Voice />} />
-                </Route>
-            </Routes>
-        </HashRouter>
-    );
+  return (
+    <>
+     <BrowserRouter>
+     <Routes>
+     <Route element={<Layout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route
+            path="dashboard"
+            element={
+              // <ProtectedRoute>
+                <Dashboard />
+              // </ProtectedRoute>
+            }
+          />
+          <Route
+            path="product"
+            element={
+              // <ProtectedRoute>
+                <Products />
+              // </ProtectedRoute>
+            }
+          />
+          <Route
+            path="anna"
+            element={
+              // <ProtectedRoute>
+                <Voice />
+              // </ProtectedRoute>
+            }
+          />
+          </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
